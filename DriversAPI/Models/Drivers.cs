53042d69587dd58181c3c80a5c08738c0f2e9045
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2024-12-09 21:33:53 +0530

    added properties ti Driver.cs

--- DriversAPI/Models/Drivers.cs
+++ DriversAPI/Models/Drivers.cs
@@ -1,6 +1,19 @@
-﻿namespace DriversAPI.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DriversAPI.Models
 {
     public class Drivers
     {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public string Team { get; set; } = string.Empty;
+
     }
 }
