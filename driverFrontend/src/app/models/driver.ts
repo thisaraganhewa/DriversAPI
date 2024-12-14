e7c786873b87c9ce54c3ce504d8e0aa872d56e05
thisaraganhewa <102313642+thisaraganhewa@users.noreply.github.com>
2024-12-14 07:53:51 +0530

    created driver module and initialize the driver service

---
+++ driverFrontend/src/app/models/driver.ts
@@ -0,0 +1,8 @@
+export class Driver{
+
+    id?: number;
+    name = "";
+    number = "";
+    team = "";
+
+}
---
+++ driverFrontend/src/app/services/driver.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Driver } from '../models/driver'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DriverService {
+
+  constructor() { }
+}
